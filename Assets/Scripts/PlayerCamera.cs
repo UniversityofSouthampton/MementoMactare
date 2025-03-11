3ddc3888c88a9acaa6eadfc464b66ad88e2ed926
Russcool <121466024+Russcool-TM@users.noreply.github.com>
2025-03-11 12:00:12 +0000

    feat: began 2nd scene, added shao's other animations, fix: clamped camera correctly
    need to use the attack scriptable objects to define the enemy sequences in the second room.
    
    also need to make it possible to go to the second room at all.
    
    as well, we need to define each sequence of 3 buttons as a move (eg. ILM is punch), so whenever we mention ILM again we actually say "punch" and the player has to remember it's ILM

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -9,6 +9,9 @@ public class PlayerCamera : MonoBehaviour
 
     [Header("Camera Values")]
     private Vector3 cameraVelocity;
+    private Vector3 cameraOffset;
+    [SerializeField] private float minX;
+    [SerializeField] private float maxX;
 
     private void Awake()
     {
@@ -25,6 +28,8 @@ public class PlayerCamera : MonoBehaviour
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
+        cameraOffset = transform.position - PlayerManager.instance.transform.position;
+        cameraOffset.z = 0;
     }
 
     public void HandleAllCameraActions()
@@ -34,7 +39,8 @@ public class PlayerCamera : MonoBehaviour
 
     private void HandleFollowTarget()
     {
-        Vector3 targetCameraPosition = Vector3.SmoothDamp(transform.position, PlayerManager.instance.transform.position, ref cameraVelocity, cameraSmoothSpeed * Time.deltaTime);
+        Vector3 targetCameraPosition = Vector3.SmoothDamp(transform.position, PlayerManager.instance.transform.position+cameraOffset, ref cameraVelocity, cameraSmoothSpeed * Time.deltaTime);
+        targetCameraPosition = new Vector3(Mathf.Clamp(targetCameraPosition.x, minX, maxX), targetCameraPosition.y, targetCameraPosition.z);
         transform.position = targetCameraPosition;
     }
 }
